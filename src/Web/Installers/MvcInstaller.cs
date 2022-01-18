dc6fef30386ec3d75d75acdb36331326e32ac772
Dima <bycodexp@gmail.com>
2022-01-18 13:59:20 +0200

    add authorization for swagger

--- src/Web/Installers/MvcInstaller.cs
+++ src/Web/Installers/MvcInstaller.cs
@@ -3,6 +3,7 @@ using FluentValidation;
 using Infrastructure;
 using Installers;
 using MediatR;
+using Microsoft.OpenApi.Models;
 using Web.Helpers;
 using Web.Services;
 
@@ -16,7 +17,33 @@ public class MvcInstaller : IInstaller
         
         services.AddEndpointsApiExplorer();
         
-        services.AddSwaggerGen();
+        services.AddSwaggerGen(options =>
+        {
+            options.SwaggerDoc("v1", new OpenApiInfo { Title = "NftMarketplace API", Version = "v1" });
+            options.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+            {
+                Name = "Authorization",
+                Type = SecuritySchemeType.ApiKey,
+                Scheme = "Bearer",
+                BearerFormat = "JWT",
+                In = ParameterLocation.Header,
+                Description = "JWT Authorization header using the Bearer scheme."
+            });
+            options.AddSecurityRequirement(new OpenApiSecurityRequirement
+            {
+                {
+                    new OpenApiSecurityScheme
+                    {
+                        Reference = new OpenApiReference
+                        {
+                            Type = ReferenceType.SecurityScheme,
+                            Id = "Bearer"
+                        }
+                    },
+                    Array.Empty<string>()
+                }
+            });
+        });
         
         services.AddTransient<IUnitOfWork, UnitOfWork>();
         
